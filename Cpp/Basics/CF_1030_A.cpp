9cf0a68ce91a040f8eab4d55a043dac616160fb0
MFRSiam <mfrforhad1@gmail.com>
2022-09-07 01:14:54 +0600

    1030_A

---
+++ Cpp/Basics/CF_1030_A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    int n;
+    std::cin >> n;
+    bool flag = false;
+    while(n--){
+        int a;
+        std::cin >> a;
+        if(a == 1){
+            std::cout << "HARD\n";
+            flag = true;
+            break;
+        }
+    }
+    if(!flag){
+        std::cout << "EASY\n";
+    }
+
+    return EXIT_SUCCESS;
+}
