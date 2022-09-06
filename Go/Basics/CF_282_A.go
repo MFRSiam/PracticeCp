7325aa01756cc9bb5507015827dfc1470418ddb4
MFRSiam <mfrforhad1@gmail.com>
2022-09-06 18:51:44 +0600

    282_A For Golang

--- Go/Basics/CF_282_A.go
+++ Go/Basics/CF_282_A.go
@@ -12,15 +12,19 @@ var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
 func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
 
-// Not Implemented The Problem
-
 func main() {
 	defer writer.Flush()
-
-	var a, b int
-	var s string
-	scanf("%d %d\n", &a, &b)
-	scanf("%s \n", &s)
-	printf("%d\n", a+b)
-	printf("%s\n", s)
+	var n int
+	var x int = 0
+	scanf("%d \n", &n)
+	for i := 0; i < n; i++ {
+		var s string
+		scanf("%s \n", &s)
+		if s[1] == '+' {
+			x++
+		} else {
+			x--
+		}
+	}
+	printf("%d\n", x)
 }
